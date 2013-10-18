090182c9d9fe5fec4951642d9ecf78ec3c43e6d5
P. Colin Manikoth <colin@manikoth.com>
2013-10-18 08:56:33 -0700

    Base Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
 // Scripts
+
+ $.simpleWeather({
+     
+     zipcode: '83814',
+     
+     unit: 'c',
+    
+     success: function(weather) {
+      
+      html = '<h1>' + weather.temp + '&deg;' + weather.units.temp + '</h1>';
+      html += '<p>' + 'Sunrise will happend at: ' + weather.sunrise + '<p>';
+         
+      //html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      //html += '<li class="currently">'+weather.currently+'</li>';
+      //html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';
+  
+      $("#weather").html(html);
+    },
+     
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  
+ });
+
+$('#weather').hide();
+$('a').click(function(){
+            
+        $('#weather').show();                
+                    
+});
