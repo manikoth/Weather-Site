17a74308b2a965f26491ba6fb4c06e7f58b67b08
P. Colin Manikoth <colin@manikoth.com>
2013-10-18 14:28:51 -0700

    Got Weather

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,24 @@ $(document).ready(function(){
         // Store Input
         var zip = $('input').val();
         console.log(zip);
+
+        // GET WEATHER
+        $.simpleWeather({
+            zipcode: '90210',
+            success: function(weather){
+                html = '<h1>' + weather.temp + '</h1>';
+                
+                // Output on Page
+                $('#weather').html(html);
+            }
+        });// end weather
         
     });//end click
     
     // Clear Input on Click
     $('input:text').click(function(){
         $('input').val('');
-    });// end clear input
+    });// end clear input    
+
     
 });// end document
