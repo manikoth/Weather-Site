0add4cab5c497f5f1f6427458575e5b5c49f3ae4
P. Colin Manikoth <colin@manikoth.com>
2013-10-18 13:51:37 -0700

    Get Zip

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,12 @@ $(document).ready(function(){
 
     // Click Button
     $('button').click(function(){
-            
-        
+    
+        // Store Input
+        var zip = $('input').val();
+        console.log(zip);
         
     });//end click
-
+    
+    
 });// end document
