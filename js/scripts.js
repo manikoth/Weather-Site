df9c2847cd3be638550a1265a394e798f8793886
P. Colin Manikoth <colin@manikoth.com>
2013-10-18 12:47:15 -0700

    Version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,41 @@
-// Scripts
+$(document).ready(function() {
+   
+    // Get Weather Button
+    $('button').click(function(){
+        
+        // STORE DATA
+            var zip = $('input').val();
 
- $.simpleWeather({
-     
-     zipcode: '83814',
-     
-     unit: 'c',
-    
-     success: function(weather) {
-      
-      html = '<h1>' + weather.temp + '&deg;' + weather.units.temp + '</h1>';
-      html += '<p>' + 'Sunrise will happend at: ' + weather.sunrise + '<p>';
-         
-      //html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-      //html += '<li class="currently">'+weather.currently+'</li>';
-      //html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';
-  
-      $("#weather").html(html);
-    },
-     
-    error: function(error) {
-      $("#weather").html('<p>'+error+'</p>');
-    }
-  
- });
-
-$('#weather').hide();
-$('a').click(function(){
+        // WEATHER
+        $.simpleWeather({
+             
+            zipcode: zip, 
+            unit: 'f',
+            // Successfully get weather
+            success: function(weather) {
+              
+                html = '<h1>' + weather.temp + '&deg;' + weather.units.temp + '</h1>';         
+                html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+                //html += '<li class="currently">'+weather.currently+'</li>';
+                //html += '<li>'+weather.tempAlt+'&deg;C</li></ul>';
             
-        $('#weather').show();                
-                    
+                // Output Weather
+                $("#weather").html(html);
+            },
+            
+            // Can't get weather? 
+            error: function(error) {
+                // Output Error Message    
+                $("#weather").html('<p>'+error+'</p>');
+            }
+                        
+        });
+        
+        // CLEAR INPUT
+        $('input:text').click(function(){
+            $(this).val('');
+        });
+
+        
+    });
 });
